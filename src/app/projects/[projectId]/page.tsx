886967eec4cf246d62bca52dd9e4373046eaa0a9
Simon Carr <simonjcarr@gmail.com>
2024-07-09 16:15:23 +0100

    add shadcn table

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,8 +1,13 @@
+import PageHeader from '@/app/components/PageHeader'
+import WidthLayout from '@/app/components/WidthLayout'
 import React from 'react'
 
 const page = ( { params } : { params: { projectId: string}} ) => {
   return (
+    <WidthLayout>
+      <PageHeader title="Project Detail" />
       <div>Project Detail page for project id {params.projectId }</div>
+    </WidthLayout>
   )
 }
 
