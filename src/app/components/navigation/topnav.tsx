ce18580192f5a6934f48c7d3cf9e9294c656c68f
Simon Carr <simonjcarr@gmail.com>
2024-07-09 16:13:25 +0100

    click logo goto home page

--- src/app/components/navigation/topnav.tsx
+++ src/app/components/navigation/topnav.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import {
     SignInButton,
@@ -9,12 +10,14 @@ import WidthLayout from '../WidthLayout'
 import { DarkModeToggle } from '@/components/darkmode-button'
 import { Button } from '@/components/ui/button'
 import { TopNavMenu } from '@/app/components/navigation/topNavMenu/TopNavMenu'
+import { useRouter } from "next/navigation"
 
 const TopNav = () => {
+    const router = useRouter()
     return (
         <WidthLayout>
             <nav className='w-full py-4 flex justify-between border-b'>
-                <div className='text-2xl font-semibold'>Meetings</div>
+                <div className='text-2xl font-semibold cursor-pointer' onClick={() => {router.push('/')}}>Meetings</div>
                 <div>
                     <SignedIn>
                         <TopNavMenu />
