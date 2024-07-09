a85916cf170695f1a8088fe0c8b9748a5713a766
Simon Carr <simonjcarr@gmail.com>
2024-07-09 17:42:16 +0100

    add project table, update .gitigore and add clerk webhook

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -8,7 +8,9 @@ import {
   serial,
   timestamp,
   varchar,
+  text
 } from "drizzle-orm/pg-core";
+
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -16,7 +18,15 @@ import {
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `meetings_${name}`);
+export const createTable = pgTableCreator((name) => `builddocs_${name}`);
+
+export const projects = createTable('project', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }),
+  description: text('description'),
+  createdAt: timestamp('created_at', { withTimezone: true }).default(sql`CURRENT_TIMESTAMP`).notNull(),
+  upldatedAt: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date()),
+})
 
 export const posts = createTable(
   "post",
